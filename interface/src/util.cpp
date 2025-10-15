a51e01ebfb9a57d41abb810845e0592ad1e44fba
Araby <arabydcr@outlook.fr>
2025-10-15 11:22:08 +0200

    resize function for TableColor

--- interface/src/util.cpp
+++ interface/src/util.cpp
@@ -33,3 +33,14 @@ TableColor generateTableColor(size_t tableColorSize) {
     }
     return tableColor;
 }
+
+TableColor resize(TableColor tableColor, size_t size) {
+    if (tableColor.size() < size) {
+        size_t missing = size - tableColor.size();
+        for (size_t i = 0; i < missing; ++i) {
+            tableColor.push_back(generateRandomColor());
+        }
+    }
+    return tableColor;
+}
+
