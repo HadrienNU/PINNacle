b75f3379068a4a0d4bf3fb3b14d865c402030674
Mathis Dubuisson <mathis.dubuisson@ecole.ensicaen.fr>
2025-11-11 18:26:41 +0100

    Seperate file

---
+++ interface/activation.py
@@ -0,0 +1,124 @@
+from interface.regions import Regions
+
+import torch
+
+
+class ActivationRegionStrategy:
+
+    def __init__(self, model, register_ready):
+        self.model = model
+        self.register_ready = register_ready
+        self.activation_storage = []
+        self.input_storage = []
+        self.map_regions_id = {}                
+        self.nb_regions = 0
+        self.resolution = 500
+        self.init_strategy()        
+
+    def init_strategy(self):
+        self.activations_output = {
+            "tanh": self.tanh_output,
+            "relu": self.relu_output
+        }
+        self.activations_pattern = {
+            "tanh": self.tanh_pattern,
+            "relu": self.relu_pattern
+        }
+
+    def get_activation_output(self):
+        return self.activations_output[self.activation_name.lower()]
+    
+    def get_activation_pattern(self):
+        return self.activations_pattern[self.activation_name.lower()]()
+    
+    def get_activation_hook(self):
+        activation_output = self.get_activation_output()
+        def get_hook(module, input, output):
+            if self.register_ready():
+                self.activation_storage.append(activation_output(output).cpu())
+        return get_hook
+    
+    def get_input(self):
+        def get_hook(module, input):
+            if self.register_ready():
+                self.input_storage.append(input[0].cpu())
+        return get_hook
+
+    def tanh_output(self, output):
+        return torch.tanh(output)
+
+    def relu_output(self, output):
+        return (output > 0).int()   
+
+    def register_hook(self):
+        self.model.net.register_forward_pre_hook(self.get_input())
+        self.activation_name = self.model.net.activation.__name__
+        print(f"Activation : {self.activation_name}")
+        get_hook = self.get_activation_hook()
+        for module in self.model.net.modules():   
+            if isinstance(module, torch.nn.modules.linear.Linear):
+                module.register_forward_hook(get_hook)
+
+    def tanh_pattern(self):
+        pass
+
+    def relu_pattern(self):
+        return torch.cat(self.activation_storage, dim=1) 
+
+    def evaluate_regions(self):
+        self.activation_storage.clear()
+        self.input_storage.clear()
+        dim = self.model.pde.geom.dim
+        if dim == 2:
+            x_range = torch.linspace(self.model.pde.bbox[0], self.model.pde.bbox[1], self.resolution)
+            y_range = torch.linspace(self.model.pde.bbox[2], self.model.pde.bbox[3], self.resolution)
+            xx, yy = torch.meshgrid(x_range, y_range, indexing='ij')
+            grid_points = torch.stack([xx.reshape(-1), yy.reshape(-1)], dim=1)
+        elif dim == 3:
+            self.resolution = 100  # Reduce resolution for 3D to limit memory usage
+            x_range = torch.linspace(self.model.pde.bbox[0], self.model.pde.bbox[1], self.resolution)
+            y_range = torch.linspace(self.model.pde.bbox[2], self.model.pde.bbox[3], self.resolution)
+            z_range = torch.linspace(self.model.pde.bbox[4], self.model.pde.bbox[5], self.resolution)
+            xx, yy, zz = torch.meshgrid(x_range, y_range, z_range, indexing='ij')
+            grid_points = torch.stack([xx.reshape(-1), yy.reshape(-1), zz.reshape(-1)], dim=1)
+        inside_mask = self.model.pde.geom.inside(grid_points.cpu().numpy())
+        valid_points = grid_points[inside_mask]
+        _ = self.model.predict(valid_points.cpu().numpy())
+
+    def export_regions(self, epoch, date):
+        self.evaluate_regions()
+        self.activation_storage.pop() # The last one does not get any activation
+        activation_pattern = self.get_activation_pattern()
+        print(activation_pattern)
+        map_region = {}
+
+        for i in range(len(activation_pattern)):
+            input_point = self.input_storage[0][i].tolist()
+            region = tuple(activation_pattern[i].tolist())
+            if region not in self.map_regions_id:
+                self.map_regions_id[region] = self.nb_regions
+                self.nb_regions += 1
+            id_region = self.map_regions_id[region]
+            if id_region in map_region:                
+                map_region[id_region].append(input_point)
+            else:                
+                map_region[id_region] = [input_point]                
+
+        geom = self.model.pde.geom
+        if geom.dim == 2:
+            regions = Regions(
+                map_region,
+                (geom.center[0], geom.center[1]),
+                geom.radius,
+                self.resolution,
+                dim=2
+            )
+        elif geom.dim == 3:
+            regions = Regions(
+                map_region,
+                (geom.center[0], geom.center[1], geom.center[2]),
+                geom.radius,
+                self.resolution,
+                dim=3
+            )
+        regions.export(f"{date}-epoch{epoch}")
