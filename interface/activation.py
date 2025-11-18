0e909f6b203afc3370e1bfb54859558c87c7757e
Mathis Dubuisson <mathis.dubuisson@ecole.ensicaen.fr>
2025-11-18 22:13:23 +0100

    elec_ritz_2d

--- interface/activation.py
+++ interface/activation.py
@@ -31,9 +31,6 @@ class Activation:
     
     def export_distances(self, name):
         plt.figure()
-        plt.plot(range(len(self.distances)), self.distances)
-        plt.savefig(f"{name}.png")
-        plt.figure()
         self.distances = sorted(list(self.distances))
         plt.plot(range(len(self.distances)), self.distances)
         plt.savefig(f"{name}_unique.png")
